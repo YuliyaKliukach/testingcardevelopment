1e1b6f8f60ba7ba19c4a9ecad15018f654319b4b
YuliyaKliukach <julia.klukach@gmail.com>
2021-06-16 13:39:54 -0400

    final red audi car at your service

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Car audi=new Car();
+        audi.color="Red";
+        audi.number="00000";
+        audi.door="3";
+        audi.model="A3";
         
     }
 }
